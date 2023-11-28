e04c00522e62de7e8e9d85790f66415372cd834a
SaraAbabneh <sara.ababneh1997@gmail.com>
2023-11-27 19:14:53 -0800

    sara

--- app/Models/Contactus.php
+++ app/Models/Contactus.php
@@ -4,8 +4,30 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Mail;
+use App\Mail\MailController; 
 
 class Contactus extends Model
 {
     use HasFactory;
+  
+    public $fillable = ['name', 'email', 'subject', 'message'];
+  
+    /**
+     * Write code on Method
+     *
+     * @return response()
+     */
+    public static function boot() {
+  
+        parent::boot();
+  
+        // This function will be triggered when a new contact is created
+        static::created(function ($item) {
+                
+            $adminEmail = "97saraababneh@gmail.com";
+            // Ensure your mail configuration in .env file is correct
+            Mail::to($adminEmail)->send(new MailController($item));
+        });
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MailController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +15,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/about', function () {
+    return view('about
+    ');
+})->name('about');
+
+
+// contact us page routes
+Route::get('/contact-us', [MailController::class, 'showContact'])->name('show.contact');
+Route::post('/contact-us', [MailController::class, 'sendContact'])->name('contact.us.store');
+
